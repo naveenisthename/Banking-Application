b05c39d592c68c9ab5baf87b44fe71ecf466c6d7
naveenisthename <108060961+naveenisthename@users.noreply.github.com>
2023-05-03 22:04:58 +0530

    Add files via upload

---
+++ src/BankingApplication/BankingApplication.java
@@ -0,0 +1,13 @@
+package BankingApplication;
+
+import java.util.Scanner;
+
+public class BankingApplication {
+
+    public static void main(String[] args) {
+
+        BankAccount first = new BankAccount("Naveenkumar", "UTR01");
+        first.showmenu();
+    }
+}
+
